48ef191bbd6f46c9c41294769f061567a2885a95
mdsiaofficial <mdsiaofficial@gmail.com>
2024-07-26 08:16:12 +0600

    "Refactored ChatList component to fetch chats from Firestore and display them, added search functionality to AddUser component"

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,12 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './ChatList.css';
 import { arrowDown, arrowUp, avatar, bg, camera, download, edit, emoji, favicon, img, info, mic, minus, more, phone, plus, search, theme, video } from '../../../assets/Images';
 import AddUser from './addUser/AddUser';
+import { useUserStore } from './../../../lib/userStore'
+import { db } from '../../../lib/firebase';
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
 
 const ChatList = () => {
 
   const [addMode, setAddMode] = useState(false);
+  const { currentUser } = useUserStore();
+  const [chats, setChats] = useState([]);
 
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "userChats", currentUser.id), async (res) => {
+      console.log("Current data: ", doc.data());
+      // setChats(doc.data());
+      const items = res.data().chats;
+
+      const promises = items.map(async (item) => {
+        const userDocRef = doc(db, "users", item.receiverId);
+        const userDocSnap = await getDoc(userDocRef);
+
+        const user = userDocSnap.data();
+
+        return { ...item, user };
+      });
+
+      const chatData = await Promise.all(promises);
+
+      setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+
+
+    });
+
+    return (() => {
+      unSub();
+    });
+  }, [currentUser.id]);
+  console.log(chats);
   return (
     <div className='chatList flex-1 no-scrollbar overflow-scroll p-3 text-white'>
       <div className="search flex items-center gap-5 p-5 ">
@@ -15,74 +47,23 @@ const ChatList = () => {
           <input type="text" placeholder='search' className='bg-transparent border-none outline-none text-white flex-1' />
         </div>
 
-        <img src={addMode ? minus : plus} alt="" className='w-8 h-8 p-1 rounded-lg bg-gray-800 cursor-pointer duration-500 transition-all' onClick={ (e) => setAddMode((prev)=> !prev)} />
+        <img src={addMode ? minus : plus} alt="" className='w-8 h-8 p-1 rounded-lg bg-gray-800 cursor-pointer duration-500 transition-all' onClick={() => setAddMode((prev) => !prev)} />
       </div>
 
-      <div className="item">
+      {chats.map((chat) => (
+        <div className="item" key={chat.chatId}>
         <img src={avatar} alt="" />
         <div className="texts">
           <span>{`Ashiq`}</span>
-          <p>{`kire koi asos tui?`}</p>
+          <p>{chat.lastMessage}</p>
         </div>
       </div>
+      ))}
+      
 
-      <div className="item">
-        <img src={avatar} alt="" />
-        <div className="texts">
-          <span>{`Ashiq`}</span>
-          <p>{`kire koi asos tui?`}</p>
-        </div>
-      </div>
 
-      <div className="item">
-        <img src={avatar} alt="" />
-        <div className="texts">
-          <span>{`Ashiq`}</span>
-          <p>{`kire koi asos tui?`}</p>
-        </div>
-      </div>
-
-      <div className="item">
-        <img src={avatar} alt="" />
-        <div className="texts">
-          <span>{`Ashiq`}</span>
-          <p>{`kire koi asos tui?`}</p>
-        </div>
-      </div>
-
-      <div className="item">
-        <img src={avatar} alt="" />
-        <div className="texts">
-          <span>{`Ashiq`}</span>
-          <p>{`kire koi asos tui?`}</p>
-        </div>
-      </div>
-
-      <div className="item">
-        <img src={avatar} alt="" />
-        <div className="texts">
-          <span>{`Ashiq`}</span>
-          <p>{`kire koi asos tui?`}</p>
-        </div>
-      </div>
-
-      <div className="item">
-        <img src={avatar} alt="" />
-        <div className="texts">
-          <span>{`Ashiq`}</span>
-          <p>{`kire koi asos tui?`}</p>
-        </div>
-      </div>
-
-      <div className="item">
-        <img src={avatar} alt="" />
-        <div className="texts">
-          <span>{`Ashiq`}</span>
-          <p>{`kire koi asos tui?`}</p>
-        </div>
-      </div>
       <div className='duration-500 transition-all'>
-      {addMode && <AddUser/>}
+        {addMode && <AddUser />}
 
       </div>
     </div>
--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -3,9 +3,21 @@ import './AddUser.css'
 import React from 'react'
 
 const AddUser = () => {
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    const { username } = formData.get("username");
+
+    try {
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className='addUser duration-700 transition-all'>
-      <form action="">
+      <form action="" onSubmit={handleSearch}>
 
         <input type="text" placeholder='Username' name='username' className='p-2 border-2 border-gray-300 rounded-md flex-1 bg-gray-800 border-none outline-none'/>
         <button className='p-2 bg-[#292929c4] hover:bg-[#47478096] border-none rounded-md cursor-pointer'>Search</button>
